2b69375ca2fcd0086cc7fa8913e7467bbcb0e00b
dany <danydevtest@gmail.com>
2024-03-04 14:40:59 -0600

    Cambios

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 
 import router from "./routes";
 import mongoose from 'mongoose';
+import cors from "cors";
 
 
 //Connection bd
@@ -15,6 +16,7 @@ const app = express()
 
 app.set('port',process.env.PORT || 4000);
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
