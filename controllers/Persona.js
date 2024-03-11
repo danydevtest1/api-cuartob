50b9bc4db0bce1a39dc0074ca5a4ea5e9acdcce7
dany <danydevtest@gmail.com>
2024-03-11 15:08:32 -0600

    Eliminar y actualizar

--- controllers/Persona.js
+++ controllers/Persona.js
@@ -22,7 +22,7 @@ export default{
             next(error);
         }
     },
-   // delPersona(req,res,next)=>{},
+   // getPersona(req,res,next)=>{},
     getPersona:async (req,res,next)=>{
        try {
         const buscar= await models.Persona.find();
@@ -33,5 +33,48 @@ export default{
         })
         next(error);
     }
+       },
+       getOnePersona:async (req,res,next)=>{
+        try {
+         const buscar= await models.Persona.findById(req.params.id);
+         res.status(200).json(buscar);
+        } catch (error) {
+         res.status(500).send({
+             message:"No se obtuvo la informacion"
+         })
+         next(error);
+     }
+        },
+       delPersona:async (req,res,next)=>{
+        try {
+            await models.Persona.findByIdAndDelete(req.params.id);
+            res.status(200).send({
+                message: "Datos eliminados correctamente"
+            })
+        } catch (error) {
+            res.status(500).send({
+                message:"No se pudo eliminar el dato"
+            })
+            next(error);
+        }
+       },
+
+       updatePersona:async(req,res,next)=>{
+        try {
+            const {nombre,apellidos, correo}=req.body;
+  
+            const actualizarDatos={
+              nombre,apellidos,correo
+          };
+          
+          const actualizar = await models.Persona.findByIdAndUpdate(req.params.id, actualizarDatos);
+          res.status(200).json(actualizar);
+              
+          } catch (error) {
+              res.status(500).send({
+                  message:"No se pudo realizar la conexion"
+              })
+              next(error);
+          }
        }
     }
--- routes/Persona.js
+++ routes/Persona.js
@@ -5,5 +5,8 @@ const router=routerx();
 
 router.post('/guardarPersona',PersonaC.postPersona);
 router.get('/buscarPersona',PersonaC.getPersona);
+router.get('/buscarOnePersona/:id',PersonaC.getOnePersona);
+router.delete('/eliminar/:id',PersonaC.delPersona);
+router.patch('/actualizar/:id',PersonaC.updatePersona);
 
 export default router;
